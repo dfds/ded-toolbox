befbed27d3e8388b99181a0475f6690580c8f483
Henrik Stanley Mortensen <hemor@dfds.com>
2019-07-08 15:01:34 +0200

    Updated to use pure PowerShell + CLI

--- update-federation-metadata/run.ps1
+++ update-federation-metadata/run.ps1
@@ -1,22 +1,20 @@
 #PowerShell version of script for easier object orrineted troubleshooting of output.
+Invoke-WebRequest -Uri "https://adfs.dfds.com/FederationMetadata/2007-06/FederationMetadata.xml" -OutFile "FederationMetadata.xml"
 
-
-(bash -c "curl https://adfs.dfds.com/FederationMetadata/2007-06/FederationMetadata.xml > FederationMetadata.xml")
-
-$Accounts = (bash -c "aws organizations list-accounts --profile saml") | ConvertFrom-Json
+$Accounts = (aws organizations list-accounts --profile saml) | ConvertFrom-Json
 
 foreach ($Account in $Accounts.Accounts) {
 
 	$ARN = "arn:aws:iam::$($Account.ID):role/OrgRole"
-    $AWS_ROLE = (bash -c "aws sts assume-role --role-arn $ARN --role-session-name Temp --profile saml") | ConvertFrom-Json
+    $AWS_ROLE = (aws sts assume-role --role-arn $ARN --role-session-name Temp --profile saml) | ConvertFrom-Json
 
-	$KEY = $AWS_ROLE.Credentials.AccessKeyId
-	$SECRET = $AWS_ROLE.Credentials.SecretAccessKey
-    $TOKEN = $AWS_ROLE.Credentials.SessionToken
+	$Env:AWS_ACCESS_KEY_ID = $AWS_ROLE.Credentials.AccessKeyId
+	$Env:AWS_SECRET_ACCESS_KEY = $AWS_ROLE.Credentials.SecretAccessKey
+    $Env:AWS_SESSION_TOKEN = $AWS_ROLE.Credentials.SessionToken
     $SAML_ARN = "arn:aws:iam::$($Account.ID):-provider/ADFS"
     
     Write-Output "Updating SAML for Account ID: $($Account.ID)"
     Write-Output "Account Name: $($Account.Name)"
 
-    (bash -c "AWS_ACCESS_KEY_ID=$KEY AWS_SECRET_ACCESS_KEY=$SECRET AWS_SESSION_TOKEN=$TOKEN aws iam update-saml-provider --saml-metadata-document file://FederationMetadata.xml --saml-provider-arn $SAML_ARN")
+    (aws iam update-saml-provider --saml-metadata-document file://FederationMetadata.xml --saml-provider-arn $SAML_ARN)
 }
