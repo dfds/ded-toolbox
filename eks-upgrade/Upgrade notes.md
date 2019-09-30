e816fec056dc58ce36496c2ae6d9b1fbb41314a7
Rasmus Rask <raras@dfds.com>
2019-09-30 10:19:16 +0200

    EKS upgrade scripts and notes

---
+++ eks-upgrade/Upgrade notes.md
@@ -0,0 +1,134 @@
+# References
+
+- https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html
+- Action required on relevant changelog, e.g. https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md
+
+## Rolling drain
+
+- Rolling drain, terminate
+  - Drain node: `k drain --ignore-daemonsets --delete-local-data --force ip-10-0-0-112.eu-west-1.compute.internal`
+  - Terminate EC2 instance
+  - Wait for EC2 instance ready
+  - Wait for Kubernetes node ready
+
+# Steps
+
+- EKS + worker AMI 1.11 --> 1.12
+  - Prime vars
+- Kubeproxy --> 1.12.6
+  - `k -n kube-system set image daemonset.apps/kube-proxy kube-proxy=602401143452.dkr.ecr.us-west-2.amazonaws.com/eks/kube-proxy:v1.12.6`
+- Kube-dns --> 1.2.2
+  - `k -n kube-system set image deployment.apps/coredns coredns=602401143452.dkr.ecr.us-west-2.amazonaws.com/eks/coredns:v1.2.2`
+- CNI --> 1.5.3
+  - `kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-1.5/config/v1.5/aws-k8s-cni.yaml`
+- Rolling drain, terminate
+
+- EKS + worker AMI 1.12 --> 1.13
+  - Prime vars
+- Kubeproxy --> 1.13.10
+  - `k -n kube-system set image daemonset.apps/kube-proxy kube-proxy=602401143452.dkr.ecr.us-west-2.amazonaws.com/eks/kube-proxy:v1.13.10`
+- Kube-dns --> 1.2.6
+  - `k -n kube-system set image deployment.apps/coredns coredns=602401143452.dkr.ecr.us-west-2.amazonaws.com/eks/coredns:v1.2.6`
+- Rolling drain, delete
+
+## Script - WIP
+
+```powershell
+$oldNodes = kubectl.exe get node --no-headers -o custom-columns=POD:metadata.name
+
+ForEach ($node in $oldNodes) {
+
+    kubectl.exe drain --ignore-daemonsets --delete-local-data --force $node
+
+    if ($?) {
+        $instance_id = aws.exe --profile oxygen --region $region ec2 describe-instances --filters "Name=private-dns-name,Values=$node" --query "Reservations[].Instances[].InstanceId" --output text
+        aws.exe --profile oxygen --region $region autoscaling set-instance-health --instance-id $instance_id --health-status Unhealthy
+    } Else {
+       Throw "Failed to drain node"
+    }
+}
+
+<#
+autoscaling describe-auto-scaling-groups
+autoscaling describe-auto-scaling-instances
+ec2 describe-instances
+autoscaling set-instance-health
+#>
+
+
+# Check health
+aws.exe --profile oxygen --region $region autoscaling describe-auto-scaling-instances --instance-ids $instance_id
+
+
+function Write-Message ([string]$Message) {
+    Write-Host "[$(Get-Date -Format u)] " -ForegroundColor Blue -NoNewLine
+    Write-Host $Message
+}
+
+
+$autoScalingGroup = 'hellman'
+$region = 'eu-west-1'
+
+$launchConfig = aws --profile oxygen --region $region autoscaling describe-auto-scaling-groups --auto-scaling-group-names $autoScalingGroup --query "AutoScalingGroups[0].LaunchConfigurationName" --output text
+Write-Message "The current launch config for autoscaling group '$autoScalingGroup' in region '$region' is '$launchConfig'"
+$rolloverInstanceQuery = aws.exe --profile oxygen --region $region autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName==``$autoScalingGroup`` && LaunchConfigurationName!=``$launchConfig``].InstanceId" --output text
+
+
+If ($rolloverInstanceQuery) {
+    $rolloverInstanceIds = $rolloverInstanceQuery.Split()
+    Write-Message "$($rolloverInstanceIds.Count) instance(s) in autoscaling group '$autoScalingGroup' in region '$region' are not using current launch config"
+} Else {
+    Write-Message "All instances in autoscaling group '$autoScalingGroup' in region '$region' appears to be using the current launch config - Nothing to do"
+    Return
+}
+
+
+$rolloverInstances = @()
+ForEach ($instanceId in $rolloverInstanceIds) {
+
+    # Attempt to resolve each EC2 instance's private DNS name (which corresponds to Kubernetes node name)
+    $nodeName = aws.exe --profile oxygen --region $region ec2 describe-instances --instance-ids $instanceId --filter "Name=private-dns-name,Values=*.*" --query "Reservations[].Instances[].PrivateDnsName" --output text 2>&1
+
+    If (-Not($?)) {
+        Write-Message "WARNING: Failed to get private DNS name of EC2 instance '$instanceId': $($Error[0].Exception.Message)"
+    }
+
+    $rolloverInstances += [PSCustomObject]@{
+        InstanceId = $instanceId
+        NodeName = "Error"
+    }
+}
+
+
+
+$rolloverNodeNames = ($rolloverInstanceIds | % {aws.exe --profile oxygen --region $region ec2 describe-instances --instance-ids $_ --filter "Name=private-dns-name,Values=*.*" --query "Reservations[].Instances[].PrivateDnsName" --output text}).Split()
+
+ForEach ($node in $rolloverNodeNames) {
+
+    # Check that node exists in cluster
+    kubectl.exe get node $node 2>&1 | Out-Null
+
+    If ($?) {
+
+        # Drain Kubernetes node
+        kubectl.exe drain --ignore-daemonsets --delete-local-data --force $node
+
+        if ($?) {
+
+            $instance_id = aws.exe --profile oxygen --region $region ec2 describe-instances --filters "Name=private-dns-name,Values=$node" --query "Reservations[].Instances[].InstanceId" --output text
+            aws.exe --profile oxygen --region $region autoscaling set-instance-health --instance-id $instance_id --health-status Unhealthy
+
+        } Else {
+
+            Throw "Failed to drain node"
+
+        }
+
+    }
+
+}
+
+
+```
+
+
---
+++ eks-upgrade/asg_k8s_rollover.ps1
@@ -0,0 +1,241 @@
+<#
+To do:
+- Add TestRun switch to switch between bogus dry-run
+- Timeout handling in loops
+    - Message
+    - Warning or Throw?
+#>
+
+[CmdletBinding()]
+
+param (
+
+    [Parameter(Mandatory = $true)]
+    [String]$AutoScalingGroup,
+
+    [Parameter(Mandatory = $false)]
+    [String]$Region = 'eu-west-1',
+
+    [Parameter(Mandatory = $false)]
+    [String]$NodeLiveTimeout = 300,
+
+    [Parameter(Mandatory = $false)]
+    [String]$NodeReadyTimeout = 300,
+
+    [switch]$TestRun
+
+)
+
+function Write-Message ([string]$Message) {
+    Write-Host "$(Get-Date -Format u)  " -ForegroundColor Blue -NoNewLine
+    Write-Host $Message
+}
+
+function Get-ASGLaunchConfig {
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [String]$AutoScalingGroup,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Region
+
+    )
+
+    $LaunchConfig = aws --profile oxygen --region $Region autoscaling describe-auto-scaling-groups --auto-scaling-group-names $AutoScalingGroup --query "AutoScalingGroups[0].LaunchConfigurationName" --output text
+    Write-Message "The current launch config for autoscaling group '$AutoScalingGroup' in region '$Region' is '$LaunchConfig'"
+    Return $LaunchConfig
+
+}
+
+
+function Get-ASGInstancesOutdatedLaunchConfig {
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [String]$AutoScalingGroup,
+
+        [Parameter(Mandatory = $true)]
+        [String]$LaunchConfig,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Region
+
+    )
+
+    # $RolloverInstanceQuery = aws --profile oxygen --region $Region autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName==``$AutoScalingGroup`` && LaunchConfigurationName!=``$LaunchConfig``].InstanceId" --output text
+    $RolloverInstanceQuery = aws --profile oxygen --region $Region autoscaling describe-auto-scaling-instances --query "AutoScalingInstances[?AutoScalingGroupName==``$AutoScalingGroup`` && LaunchConfigurationName==``$LaunchConfig``].InstanceId" --output text # debug
+
+    If ($RolloverInstanceQuery) {
+        Write-Message "$($RolloverInstanceIds.Count) instance(s) in autoscaling group '$AutoScalingGroup' in region '$Region' are not using current launch config"
+        Return $RolloverInstanceQuery.Split()
+    }
+    Else {
+        Write-Message "All instances in autoscaling group '$AutoScalingGroup' in region '$Region' appears to be using the current launch config - Nothing to do"
+        Return
+    }
+
+}
+
+
+function Get-InstancePrivateDnsName {
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [String]$Id,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Region
+
+    )
+
+    $PrivateDnsName = aws --profile oxygen --region $Region ec2 describe-instances --instance-ids $InstanceId --filter "Name=private-dns-name,Values=*.*" --query "Reservations[].Instances[].PrivateDnsName" --output text 2>&1
+
+    If ($?) {
+        Write-Message "Instance '$InstanceId' in region '$Region' has a private DNS name of '$PrivateDnsName'"
+        Return $PrivateDnsName
+    }
+    Else {
+        Write-Message "WARNING: Failed to get private DNS name of EC2 instance '$InstanceId': $($Error[0].Exception.Message)"
+        Return
+    }
+}
+
+
+function Get-KubernetesNodes {
+    kubectl get node --no-headers -o custom-columns=POD:metadata.name
+}
+
+function Get-KubernetesNodeReady {
+    
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [String[]]$Name
+
+    )
+
+    $Name | ForEach {
+        [PSCustomObject]@{
+            Node  = $_
+            Ready = (kubectl get nodes -o json | jq -r --arg NODENAME $_ '[.items[] | {id: .metadata.name,status: (.status.conditions[] | select(.type==\"Ready\").status)}] | .[] | select( .id | contains("$NODENAME")).status') -eq 'True'
+        }
+    }
+
+}
+
+function Set-KubernetesNodeDrain {
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [String]$Name
+
+    )
+
+    Write-Message "Draining Kubernetes node '$Name'"
+    
+    # $Result = kubectl drain --ignore-daemonsets --delete-local-data --force $Name 2>&1
+    # $Result = kubectl drain $Name 2>&1 #debug
+    $Result = kubectl version --client #debug
+
+    If ($LASTEXITCODE -ne 0) {
+
+        Write-Message "WARNING: Failed to drain node '$Name': $Result"
+
+        Write-Message "Uncordoning node '$Name'"
+        $Result = kubectl uncordon $Name 2>&1
+
+        If ($LASTEXITCODE -ne 0) {
+            Throw "Failed to uncordon, after failing to drain, Kubernetes node '$Name': $Result"
+        }
+
+        Return $false
+
+    }
+
+    Return $true
+
+}
+
+function Set-InstanceHealthStatus {
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]$Id,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Region,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("Unhealthy")]
+        [String]$Status
+
+    )
+
+    Write-Message "Setting instance '$Id' in region '$Region' to 'Unhealthy'"
+    # aws --profile oxygen --region $Region autoscaling set-instance-health --instance-id $Id --health-status $Status
+
+}
+
+
+$LaunchConfig = Get-ASGLaunchConfig -AutoScalingGroup $AutoScalingGroup -Region $Region
+
+$RolloverInstanceIds = Get-ASGInstancesOutdatedLaunchConfig -AutoScalingGroup $AutoScalingGroup -LaunchConfig $LaunchConfig -Region $Region
+
+$RolloverInstances = @()
+ForEach ($InstanceId in $RolloverInstanceIds) {
+
+    # Attempt to resolve each EC2 instance's private DNS name (which corresponds to Kubernetes node name)
+    $NodeName = Get-InstancePrivateDnsName -Id $InstanceId -Region $Region
+
+    If ($NodeName) {
+        $RolloverInstances += [PSCustomObject]@{
+            Id       = $InstanceId
+            NodeName = $NodeName
+        }
+    }
+
+}
+
+
+ForEach ($Instance in $RolloverInstances) {
+
+    # Get current Kubernets nodes
+    $PreviousNodes = Get-KubernetesNodes
+
+    # Drain Kubernetes node
+    If (Set-KubernetesNodeDrain -Name $Instance.NodeName) {
+   
+        # Set EC2 instance health to unhealthy to trigger replacement
+        Set-InstanceHealthStatus -Id $Instance.Id -Region $Region -Status Unhealthy
+
+        # Wait for new Kubernetes node to appear
+        $i = 0
+        Do {
+            Start-Sleep 1 # 10
+            $CurrentNodes = Get-KubernetesNodes
+            # $NewNodes = Compare-Object -ReferenceObject $PreviousNodes -DifferenceObject $CurrentNodes -PassThru
+            # $NewNodes = Compare-Object -ReferenceObject $PreviousNodes -DifferenceObject ($CurrentNodes + 'ip-99-99-99-99.eu-west-1.compute.internal') -PassThru # debug
+            $Newnodes = @('ip-10-0-1-237.eu-west-1.compute.internal', 'ip-10-0-1-9.eu-west-1.compute.internal') #debug
+        }
+        Until ($NewNodes -or ($i++ -ge 3))
+        Write-Message "New Kubernetes node joined: $($NewNodes -join ', ')"
+    
+        # Wait for new Kubernetes node to be ready
+        Do {
+            Start-Sleep 1 # 10
+            $NodeStatus = Get-KubernetesNodeReady -Name $NewNodes
+        }
+        Until ($NodeStatus.Ready -notcontains $false)
+        Write-Message "Kubernetes node '$($NewNodes -join ', ')' is now ready"
+   
+    }
+
+}
+
+
+Get-ASGInstancesOutdatedLaunchConfig -AutoScalingGroup $AutoScalingGroup -LaunchConfig $LaunchConfig -Region $Region | Out-Null
