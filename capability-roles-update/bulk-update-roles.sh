7d55d33219e78ceb8926a6a39c1acf1375857db0
Willi Carlsen <carlsenwilli@gmail.com>
2020-06-08 15:35:10 +0200

    Updated script and made it more safe when failure occurs

--- capability-roles-update/bulk-update-roles.sh
+++ capability-roles-update/bulk-update-roles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 aws s3 cp s3://dfds-oxygen-k8s-hellman/capability-role.yaml ./capability-role.yaml
 
 ROLES=$(kubectl get roles --all-namespaces -o=custom-columns='DATA:metadata.name' | grep 'fullaccess')
