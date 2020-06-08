57e97211dbf0349426e69cb6f9e74430ebe7d792
Willi Carlsen <carlsenwilli@gmail.com>
2020-06-08 15:24:53 +0200

    Created a bulk update capability roles script fetching role from a central S3 bucket

---
+++ capability-roles-update/README.md
@@ -0,0 +1,8 @@
+# Bulk update capability roles
+
+Run in AWS oxygen account context.
+
+```bash
+export AWS_PROFILE=YOUR_OXYGEN_CONTEXT
+./bulk-update-roles.sh
+```
---
+++ capability-roles-update/bulk-update-roles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws s3 cp s3://dfds-oxygen-k8s-hellman/capability-role.yaml ./capability-role.yaml
+
+ROLES=$(kubectl get roles --all-namespaces -o=custom-columns='DATA:metadata.name' | grep 'fullaccess')
+
+for ROLE in $ROLES
+do
+	NAMESPACE=${ROLE%*-fullaccess}
+	echo "Applying role updates in namespace $NAMESPACE"
+	sed "s/CAPABILITY_ROOT_ID/${NAMESPACE}/g" capability-role.yaml | kubectl replace -f -
+done
