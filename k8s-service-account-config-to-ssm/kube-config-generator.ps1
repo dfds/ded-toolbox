e50d960e371c6e4ff3bbef0b0b4933f6f181819b
Peter West <pwes@DFDS.COM>
2020-10-13 13:26:57 +0100

    Modifications to .ps1 script in k8s-service-account-config-to-ssm to support new security model

--- k8s-service-account-config-to-ssm/kube-config-generator.ps1
+++ k8s-service-account-config-to-ssm/kube-config-generator.ps1
@@ -8,11 +8,7 @@ param (
 
     [Parameter(Mandatory)]
     [string]
-    $AccountId,
-
-    [Parameter()]
-    [string]
-    $AwsProfile
+    $AccountId
 )
 
 # Load include file
@@ -30,12 +26,19 @@ $ServiceAccountName = "${CapabilityRootId}-vstsuser"
 $ServiceAccountNamespace = 'kube-system'
 $KubeRole = "${CapabilityRootId}-fullaccess"
 $CapabilityAwsAccountId = $AccountId
-$CapabilttiAwsRoleArn = "arn:aws:iam::${CapabilityAwsAccountId}:role/OrgRole"
+$CapabilityAwsRoleArn = "arn:aws:iam::${CapabilityAwsAccountId}:role/OrgRole"
 $CapabilityAwsRoleSession = "kube-config-paramstore"
 $AwsRegion = 'eu-central-1'
 $AwsParameterName = '/managed/deploy/kube-config'
+$AwsProfile = "saml"
+
+# SAML2AWS Connection
+$SamlRole = "arn:aws:iam::738063116313:role/CloudAdmin"
+saml2aws login --role=$SamlRole --force
 
 # Generate kube toke and config for service account
+$ENV:AWS_PROFILE = $AwsProfile
+
 kubectl --namespace $ServiceAccountNamespace create serviceaccount $ServiceAccountName
 kubectl --namespace $Namespace create rolebinding $ServiceAccountName --role=$Kuberole --serviceaccount=${ServiceAccountNamespace}:${ServiceAccountName}
 
@@ -45,12 +48,14 @@ $KubeToken = $(kubectl --namespace $ServiceAccountNamespace get secret $KubeSecr
 $KubeConfigTemplate = Get-Content (Join-Path $ScriptRoot 'config.template') -Raw
 $KubeConfig = $KubeConfigTemplate -replace 'NAMESPACE_REPLACE', $Namespace -replace 'KUBE_TOKEN', $KubeToken
 
+# SAML2AWS Connection
+$SamlRole = "arn:aws:iam::454234050858:role/ADFS-Admin"
+saml2aws login --role=$SamlRole --force
+
 # Assume role
 Set-DefaultAWSRegion -Region $AwsRegion
-If ($AwsProfile) {    
-    Set-AWSCredential -ProfileName $AwsProfile
-}
-Try {$Creds = Use-STSRole -RoleArn $CapabilttiAwsRoleArn -RoleSessionName Push-KubeConfig | Select-Object -Expand Credentials}
+Set-AWSCredential -ProfileName $AwsProfile
+Try { $Creds = Use-STSRole -RoleArn $CapabilityAwsRoleArn -RoleSessionName $CapabilityAwsRoleSession | Select-Object -Expand Credentials}
 Catch {Throw "Failed to assume role: ($_.Message)"}
 Set-AWSCredential -AccessKey $Creds.AccessKeyId -SecretKey $Creds.SecretAccessKey -SessionToken $Creds.SessionToken
 
