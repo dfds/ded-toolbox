2412884b0d6315523765a2ce629783ccf43d0946
ZaradarDFDS <54835628+ZaradarDFDS@users.noreply.github.com>
2021-05-25 10:38:48 +0200

    Microsoft graph powershell hotfix (#9)
    * Simple container workload to run powershell on ubuntu with MS Graph CLI pre-installed
    
    * renaming folder
    
    * Implemented support for AzureAD cli modules in powershell core via MS internal package used by Azure Cloud CLI
    
    * Updating README
    
    * Make ExtensionName param mandatory
    
    * Added script for assigning directory extension values to a user
    
    * changed readme to use guid.empty

---
+++ microsoft-ubuntu-powershell/scripts/add_user_directory_extension.ps1
@@ -0,0 +1,18 @@
+﻿#Requires -Modules AzureAD.Standard.Preview
+
+param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)] 
+    [string]$UserObjectId,
+
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)] 
+    [string]$ExtensionName,
+
+    [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)] 
+    [string]$ExtensionValue
+)
+
+# Connect to AzureAD 
+Connect-AzureAD
+
+# Add extension property value to user
+Set-AzureADUserExtension -ObjectId $UserObjectId -ExtensionName $ExtensionName -ExtensionValue $ExtensionValue
---
+++ microsoft-ubuntu-powershell/scripts/register_directory_extension_property.ps1
@@ -0,0 +1,76 @@
+﻿#Requires -Modules AzureAD.Standard.Preview
+
+param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)] 
+    [string]$AzureADApplicationId,
+
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)] 
+    [string]$ExtensionName,
+
+    [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)] 
+    [ValidateSet('String', 'string', 'Integer', 'integer')]
+    [string]$ExtensionDataType = "string"
+)
+
+# Support functions
+function GetExtensionProperty{
+    Param( 
+        [Parameter(Mandatory=$true)]
+        [String]$ExtensionName,
+
+        [Parameter(Mandatory=$true)]
+        [String]$OwnerId
+    )
+
+    $extension = (Get-AzureADApplicationExtensionProperty -ObjectId $OwnerId).Where( { $_.Name.endsWith($ExtensionName)})[0]
+
+    return @{ 
+        "JwtClaimType" = $ExtensionName 
+        "ExtensionId"  = $extension.Name
+    }
+}
+
+# Connect to AzureAD 
+Connect-AzureAD
+
+# Fetch app registration
+$AppRegistration = Get-AzureADApplication -Filter "AppId eq '$AzureADApplicationId'"
+
+# Fetch extension property meta data
+$AppExtensionProperty = GetExtensionProperty -ExtensionName $ExtensionName -OwnerId $AppRegistration.ObjectId
+
+# If meta data does not contain an Id the extension property needs to be created
+if($AppExtensionProperty.ExtensionId -eq $null){
+    # Create a new extension property in Azure AD Graph
+    New-AzureADApplicationExtensionProperty -ObjectId $AppRegistration.ObjectId -Name $ExtensionName -DataType $ExtensionDataType -TargetObjects @("User")
+
+    # Reacquire extension property meta data to populate ExtensionId
+    $AppExtensionProperty = GetExtensionProperty -ExtensionName $ExtensionName -OwnerId $AppRegistration.ObjectId
+
+    # Configure claims mapping policy for app registration service principal
+    $ClaimsMappingPolicy = [ordered]@{
+      "ClaimsMappingPolicy" = [ordered]@{
+        "Version"              = 1
+        "IncludeBasicClaimSet" = $true
+        "ClaimsSchema"         = @(
+          [ordered]@{
+            "Source"       = "User"
+            "ExtensionID"  = $ExtensionReference.ExtensionId
+            "JwtClaimType" = $ExtensionReference.JwtClaimType
+          }
+        )
+      }
+    }
+
+    # Fetch service principal of app registration
+    $SecurityPrincipal = (Get-AzureADServicePrincipal -Filter "Id eq '$AzureADApplicationId'") 
+    
+    # Convert hashtable to json
+    $PolicyDefinition = $ClaimsMappingPolicy | ConvertTo-Json -Depth 99 -Compress
+
+    # Create new Azure AD policy object with new policy
+    $Policy = New-AzureADPolicy -Type "ClaimsMappingPolicy" -DisplayName $AppRegistration.DisplayName -Definition $PolicyDefinition
+
+    # Add policy to app registration service principal
+    Add-AzureADServicePrincipalPolicy -Id $SecurityPrincipal.ObjectId -RefObjectId $Policy.Id
+}
