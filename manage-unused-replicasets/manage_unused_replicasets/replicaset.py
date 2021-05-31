ca91d8a2faeb49ce82b6d7a6400a6aaacbb55cd8
Audun Nes <aunes@dfds.com>
2021-05-31 12:17:46 +0200

    Fix bug in reporting + add code for cleaning up

--- manage-unused-replicasets/manage_unused_replicasets/replicaset.py
+++ manage-unused-replicasets/manage_unused_replicasets/replicaset.py
@@ -39,7 +39,7 @@ class ReplicaSet:
             replicasets.append(name)
         return replicasets
 
-    def get_unused_replicasets(self, namespace: str) -> list:
+    def get_unused_replicasets(self, namespace: str, deployment_name: str) -> list:
         replicasets_spec = self._get_replicasets_spec(namespace)
         replicasets: list = []
         for item in replicasets_spec:
@@ -52,33 +52,16 @@ class ReplicaSet:
             ready_replicas: int = status.get('readyReplicas', 0)
             replicas: int = status.get('replicas')  # This will always have a value
             if available_replicas == 0 and ready_replicas == 0 and replicas == 0:
-                name: str = metadata.get('name')
-                replicasets.append(name)
-        return replicasets
-
-    def get_unused_replicasets_for_deployment(self, namespace: str, deployment_name: str) -> list:
-        replicasets_spec = self._get_replicasets_spec(namespace)
-        replicasets: list = []
-        for item in replicasets_spec:
-            item_with_type_hints: dict = item
-            metadata: dict = item_with_type_hints.get('metadata')
-            status: dict = item_with_type_hints.get('status')
-            # This will only have a value if there are available replicas
-            available_replicas: int = status.get('availableReplicas', 0)
-            # This will only have a value if there are ready replicas
-            ready_replicas: int = status.get('readyReplicas', 0)
-            replicas: int = status.get('replicas')  # This will always have a value
-            if available_replicas == 0 and ready_replicas == 0 and replicas == 0:
-                owner_references: list = metadata.get("ownerReferences")
-                application: str = owner_references[0]['name']
-                if (application == deployment_name):
-                    name: str = metadata.get('name')
-                    replicasets.append(name)
+                try:
+                    owner_references: list = metadata.get("ownerReferences")
+                    application: str = owner_references[0]['name']
+                    if (application == deployment_name):
+                        name: str = metadata.get('name')
+                        replicasets.append(name)
+                except TypeError:
+                    print('The ownerReferences field is empty. Skipping.')
         return replicasets
 
     def get_number_of_unused_replicasets_per_deployment(self, namespace: str, deployment_name: str) -> int:
-        unused_replicasets: list = self.get_unused_replicasets_for_deployment(namespace, deployment_name)
+        unused_replicasets: list = self.get_unused_replicasets(namespace, deployment_name)
         return len(unused_replicasets)
-
-    def print_number_of_unused_replicasets_per_deployment(self, namespace: str, deployment_name: str) -> int:
-        replicasets: list = []
