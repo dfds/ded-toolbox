4bddcaefa84eafa6274a8e498a5f1ea3b9a5f2f4
Audun Nes <aunes@dfds.com>
2021-06-01 13:09:28 +0200

    Use black as development dependency

--- kafka-delete-topic/capability_service/kafka.py
+++ kafka-delete-topic/capability_service/kafka.py
@@ -9,28 +9,34 @@ from requests import Response
 class InvalidKafkaCluster(Exception):
     """Exception raised if trying to use a non-existing Kafka cluster."""
 
-    def __init__(self, environment, message="Environment is not a valid Kafka cluster. Use 'dev' or 'prod'"):
+    def __init__(
+        self,
+        environment,
+        message="Environment is not a valid Kafka cluster. Use 'dev' or 'prod'",
+    ):
         self.environment = environment
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
-        return f'{self.environment} -> {self.message}'
+        return f"{self.environment} -> {self.message}"
 
 
 class Topic:
     """A class that represent a Kafka topic."""
 
-    def __init__(self,
-                 blaster_bearer_token: str,
-                 blaster_topics_api_url: str,
-                 blaster_cluster_uuid_prod: str,
-                 blaster_cluster_uuid_dev: str,
-                 confluent_env_id_prod: str,
-                 confluent_env_id_dev: str,
-                 confluent_cluster_id_prod: str,
-                 confluent_cluster_id_dev: str,
-                 level: int = logging.INFO) -> None:
+    def __init__(
+        self,
+        blaster_bearer_token: str,
+        blaster_topics_api_url: str,
+        blaster_cluster_uuid_prod: str,
+        blaster_cluster_uuid_dev: str,
+        confluent_env_id_prod: str,
+        confluent_env_id_dev: str,
+        confluent_cluster_id_prod: str,
+        confluent_cluster_id_dev: str,
+        level: int = logging.INFO,
+    ) -> None:
         """Class constructor.
         :param blaster_bearer_token: A Bearer token which can authenticate with the capability service.
         :param blaster_topics_api_url: The full URL to the Topic API, for instance https://<hostname>/capability/api/v1/topics # noqa 501
@@ -51,7 +57,9 @@ class Topic:
         :type confluent_cluster_id_dev: str
         :type level: int
         """
-        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=level)
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=level
+        )
         self.blaster_bearer_token: str = blaster_bearer_token
         self.blaster_topics_api_url: str = blaster_topics_api_url
         self.blaster_cluster_uuid_prod: str = blaster_cluster_uuid_prod
@@ -68,9 +76,9 @@ class Topic:
         :type environment_name: str
         :return: str
         """
-        if environment_name.upper() in ['PROD', 'PRODUCTION']:
+        if environment_name.upper() in ["PROD", "PRODUCTION"]:
             return self.blaster_cluster_uuid_prod
-        elif environment_name.upper() in ['DEV', 'DEVELOP', 'DEVELOPMENT']:
+        elif environment_name.upper() in ["DEV", "DEVELOP", "DEVELOPMENT"]:
             return self.blaster_cluster_uuid_dev
         else:
             raise InvalidKafkaCluster(environment_name)
@@ -81,9 +89,9 @@ class Topic:
         :type environment_name: str
         :return: str
         """
-        if environment_name.upper() in ['PROD', 'PRODUCTION']:
+        if environment_name.upper() in ["PROD", "PRODUCTION"]:
             return self.confluent_env_id_prod
-        elif environment_name.upper() in ['DEV', 'DEVELOP', 'DEVELOPMENT']:
+        elif environment_name.upper() in ["DEV", "DEVELOP", "DEVELOPMENT"]:
             return self.confluent_env_id_dev
         else:
             raise InvalidKafkaCluster(environment_name)
@@ -94,9 +102,9 @@ class Topic:
         :type environment_name: str
         :return: str
         """
-        if environment_name.upper() in ['PROD', 'PRODUCTION']:
+        if environment_name.upper() in ["PROD", "PRODUCTION"]:
             return self.confluent_cluster_id_prod
-        elif environment_name.upper() in ['DEV', 'DEVELOP', 'DEVELOPMENT']:
+        elif environment_name.upper() in ["DEV", "DEVELOP", "DEVELOPMENT"]:
             return self.confluent_cluster_id_dev
         else:
             raise InvalidKafkaCluster(environment_name)
@@ -110,34 +118,49 @@ class Topic:
         self._delete_confluent_topics(topics)
 
     def _delete_blaster_topics(self, topics: dict) -> None:
-        headers: dict = {'Authorization': f'Bearer {self.blaster_bearer_token}'}
+        headers: dict = {"Authorization": f"Bearer {self.blaster_bearer_token}"}
         for topic_name, environment_name in topics.items():
             uuid: str = self._get_cluster_uuid(environment_name)
-            url: str = f'{self.blaster_topics_api_url}/{topic_name}?clusterId={uuid}'
-            logging.debug(f'Calling {url}')
+            url: str = f"{self.blaster_topics_api_url}/{topic_name}?clusterId={uuid}"
+            logging.debug(f"Calling {url}")
             response: Response = requests.delete(url=url, headers=headers)
             if response.status_code == 200:
-                logging.info(f'Topic {topic_name} was deleted from capability service.')
+                logging.info(f"Topic {topic_name} was deleted from capability service.")
             elif response.status_code == 400:
-                logging.warning(f'Topic {topic_name} was not found on capability service.')
+                logging.warning(
+                    f"Topic {topic_name} was not found on capability service."
+                )
             elif response.status_code == 401:
-                logging.warning('You are not authenticated to the capability service. '
-                                'Please check the value of your BLASTER_BEARER_TOKEN')
+                logging.warning(
+                    "You are not authenticated to the capability service. "
+                    "Please check the value of your BLASTER_BEARER_TOKEN"
+                )
             else:
-                logging.error(f'The capability service returned status code {response.status_code}')
+                logging.error(
+                    f"The capability service returned status code {response.status_code}"
+                )
 
     def _delete_confluent_topics(this, topics: dict) -> None:
         """Delete topics from Confluent cloud using the ccloud tool.
         :param topics: A dictionary of topics and uuids (from the capability service)
         :type topics: dict
         """
-        logging.info('Delete topics on Confluent Cloud.')
-        subprocess.run(['ccloud', 'login'])
+        logging.info("Delete topics on Confluent Cloud.")
+        subprocess.run(["ccloud", "login"])
         for topic_name, environment_name in topics.items():
             env_id: str = this._get_confluent_environment_id(environment_name)
             cluster_id: str = this._get_confluent_cluster_id(environment_name)
-            delete_topic_cmd: list = ['ccloud', 'kafka', 'topic', 'delete',
-                                      topic_name, '--environment', env_id, '--cluster', cluster_id]
+            delete_topic_cmd: list = [
+                "ccloud",
+                "kafka",
+                "topic",
+                "delete",
+                topic_name,
+                "--environment",
+                env_id,
+                "--cluster",
+                cluster_id,
+            ]
             logging.info(f'{" ".join(delete_topic_cmd)}')
             subprocess.run(delete_topic_cmd)
 
@@ -148,11 +171,13 @@ class Topic:
         :type topics: dict
         """
         admin_client: AdminClient = AdminClient(config)
-        futures: dict = admin_client.delete_topics(list(topics.keys()), operation_timeout=30)
+        futures: dict = admin_client.delete_topics(
+            list(topics.keys()), operation_timeout=30
+        )
 
         for topic_name, future in futures.items():
             try:
                 future.result()
-                logging.info(f'Topic {topic_name} deleted')
+                logging.info(f"Topic {topic_name} deleted")
             except Exception as e:
-                logging.error(f'Failed to delete topic {topic_name}: {e}')
+                logging.error(f"Failed to delete topic {topic_name}: {e}")
--- kafka-delete-topic/delete_topics.py
+++ kafka-delete-topic/delete_topics.py
@@ -9,29 +9,33 @@ from capability_service.kafka import Topic
 def main():
     load_dotenv()
 
-    blaster_bearer_token: str = os.environ.get('BLASTER_BEARER_TOKEN')
-    blaster_topics_api_url: str = os.environ.get('BLASTER_TOPICS_API_URL')
-    blaster_cluster_uuid_prod: str = os.environ.get('BLASTER_CLUSTER_UUID_PROD')
-    blaster_cluster_uuid_dev: str = os.environ.get('BLASTER_CLUSTER_UUID_DEV')
+    blaster_bearer_token: str = os.environ.get("BLASTER_BEARER_TOKEN")
+    blaster_topics_api_url: str = os.environ.get("BLASTER_TOPICS_API_URL")
+    blaster_cluster_uuid_prod: str = os.environ.get("BLASTER_CLUSTER_UUID_PROD")
+    blaster_cluster_uuid_dev: str = os.environ.get("BLASTER_CLUSTER_UUID_DEV")
 
-    confluent_env_id_prod: str = os.environ.get('CONFLUENT_ENV_ID_PROD')
-    confluent_env_id_dev: str = os.environ.get('CONFLUENT_ENV_ID_DEV')
-    confluent_cluster_id_prod: str = os.environ.get('CONFLUENT_CLUSTER_ID_PROD')
-    confluent_cluster_id_dev: str = os.environ.get('CONFLUENT_CLUSTER_ID_DEV')
+    confluent_env_id_prod: str = os.environ.get("CONFLUENT_ENV_ID_PROD")
+    confluent_env_id_dev: str = os.environ.get("CONFLUENT_ENV_ID_DEV")
+    confluent_cluster_id_prod: str = os.environ.get("CONFLUENT_CLUSTER_ID_PROD")
+    confluent_cluster_id_dev: str = os.environ.get("CONFLUENT_CLUSTER_ID_DEV")
 
-    topic: Topic = Topic(blaster_bearer_token=blaster_bearer_token,
-                         blaster_topics_api_url=blaster_topics_api_url,
-                         blaster_cluster_uuid_prod=blaster_cluster_uuid_prod,
-                         blaster_cluster_uuid_dev=blaster_cluster_uuid_dev,
-                         confluent_env_id_prod=confluent_env_id_prod,
-                         confluent_env_id_dev=confluent_env_id_dev,
-                         confluent_cluster_id_prod=confluent_cluster_id_prod,
-                         confluent_cluster_id_dev=confluent_cluster_id_dev)
+    topic: Topic = Topic(
+        blaster_bearer_token=blaster_bearer_token,
+        blaster_topics_api_url=blaster_topics_api_url,
+        blaster_cluster_uuid_prod=blaster_cluster_uuid_prod,
+        blaster_cluster_uuid_dev=blaster_cluster_uuid_dev,
+        confluent_env_id_prod=confluent_env_id_prod,
+        confluent_env_id_dev=confluent_env_id_dev,
+        confluent_cluster_id_prod=confluent_cluster_id_prod,
+        confluent_cluster_id_dev=confluent_cluster_id_dev,
+    )
 
     # This is where you need to add the topics you want to delete.
-    topics_data: dict = {'pub.sandbox-aunes-kkpbj.foo': 'dev',
-                         'pub.sandbox-aunes-kkpbj.bar': 'dev',
-                         'sandbox-aunes-kkpbj.lala': 'prod'}
+    topics_data: dict = {
+        "pub.sandbox-aunes-kkpbj.foo": "dev",
+        "pub.sandbox-aunes-kkpbj.bar": "dev",
+        "sandbox-aunes-kkpbj.lala": "prod",
+    }
 
     topic.delete_topics(topics_data)
 
