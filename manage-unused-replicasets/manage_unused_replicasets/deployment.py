0ae0a9af5bf88612180a7be89685691a936f46fe
Audun Nes <aunes@dfds.com>
2021-06-01 12:06:53 +0200

    create_script_to_patch_deployments

--- manage-unused-replicasets/manage_unused_replicasets/deployment.py
+++ manage-unused-replicasets/manage_unused_replicasets/deployment.py
@@ -68,3 +68,32 @@ class Deployment:
         header = deployments[0].keys()
         rows = [deployment.values() for deployment in deployments]
         print(tabulate(rows, header))
+
+    def create_script_to_patch_deployments(self, deployments: list = None) -> None:
+        """
+        Create script to use 'kubectl patch deployments' in order to reduce the
+        revisionHistoryLimit in deployments with a revisionHistoryLimit higher than
+        the default value of 10.
+
+        :param deployments: A list of dictionary objects that contains the namespace,
+                            deployment name, api version and revisionHistoryLimit value
+                            for deployments that have a high revisionHistoryLimit.
+                            If not supplied, it will create the list of its own.
+        :type deployments: list
+        """
+        dp: list = []
+        if deployments is None:
+            dp = self.get_deployments_with_high_revision_history_limit()
+        else:
+            dp = deployments
+        script_file: str = 'problematic-deployments.sh.tmp'
+        with open(script_file, 'w') as writer:
+            writer.write('!#/bin/bash\n')
+            patch: str = '\'[{"op": "replace", "path": "/spec/revisionHistoryLimit", "value": 10}]\''
+            for deployment in dp:
+                namespace: str = deployment.get('namespace')
+                name: str = deployment.get('name')
+                writer.write(f'kubectl patch deployment -n {namespace} {name} --type=json -p={patch}\n')
+        print(f'{script_file} was created with "kubectl patch deployments" commands')
+        print('You can use this script to change the revisionHistoryLimit.')
+        print(f'Usage: export KUBECONFIG=<REDACTED> && chmod +x {script_file} && ./{script_file}')
